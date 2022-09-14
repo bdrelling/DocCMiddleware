08619015a387bbeae597495aed6e07883089c87e
Brian Drelling <brian@drelling.com>
2022-09-14 17:50:38 -0500

    Added iOS, tvOS, and watchOS to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "DocCMiddleware",
     platforms: [
+        .iOS(.v15),
         .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
     ],
     products: [
         .library(name: "DocCMiddleware", targets: ["DocCMiddleware"]),
